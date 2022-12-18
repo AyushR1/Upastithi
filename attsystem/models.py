e6a16f3e81b373544ed55a46f79eda284b4433f5
AyushR1 <ayushrathore12501@gmail.com>
2022-12-18 15:53:26 +0530

    Added Edit Page for Student,Staff,Subject,Course
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- attsystem/models.py
+++ attsystem/models.py
@@ -34,11 +34,7 @@ class Subjects(models.Model):
     id=models.AutoField(primary_key=True)
     subject_name=models.CharField(max_length=255)
     course_id=models.ForeignKey(Courses,on_delete=models.CASCADE,default=1)
-<<<<<<< HEAD:attsystem/models.py
     staff_id=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-=======
-    staff_id=models.ForeignKey(Staffs,on_delete=models.CASCADE)
->>>>>>> a7abd2c... Added Login Functionality For Admin,Staff,Students:student_management_app/models.py
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
