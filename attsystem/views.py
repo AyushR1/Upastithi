064784d2b4e67c2c79ace5f3c8eed219c7ce953f
AyushR1 <ayushrathore12501@gmail.com>
2022-12-24 19:33:08 +0530

    Revert Captcha
    This is killing me in testing
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- attsystem/views.py
+++ attsystem/views.py
@@ -1,8 +1,6 @@
 import datetime
 import os
-import json
 
-import requests
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.storage import FileSystemStorage
@@ -24,17 +22,6 @@ def doLogin(request):
     if request.method!="POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        captcha_token=request.POST.get("g-recaptcha-response")
-        cap_url="https://www.google.com/recaptcha/api/siteverify"
-        cap_secret="6LeWtqUZAAAAANlv3se4uw5WAg-p0X61CJjHPxKT"
-        cap_data={"secret":cap_secret,"response":captcha_token}
-        cap_server_response=requests.post(url=cap_url,data=cap_data)
-        cap_json=json.loads(cap_server_response.text)
-
-        if cap_json['success']==False:
-            messages.error(request,"Invalid Captcha Try Again")
-            return HttpResponseRedirect("/")
-
         user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
         if user!=None:
             login(request,user)
